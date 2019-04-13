508523f50e067b167ab8943839ea829cc74a3f57
emmanuel-82 <slidefix82@gmail.com>
2019-04-13 17:46:34 +0100

    Switching to master

--- src/test/java/stepdefinition/SearchStep.java
+++ src/test/java/stepdefinition/SearchStep.java
@@ -3,24 +3,30 @@ package stepdefinition;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SearchStep {
 
     @Given("^I an on Google Search page$")
     public void i_an_on_Google_Search_page() throws Throwable {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\TriNity\\IdeaProjects\\ACUKJATTraining\\Drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.firefox.marionette", "C:\\Users\\TriNity\\IdeaProjects\\ACUKJATTraining\\Drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\TriNity\\IdeaProjects\\ACUKJATTraining\\Drivers\\chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
 
+        WebElement element  = driver.findElement(By.name("q"));
+        element.sendKeys("apple");
 
      }
 
     @When("^I enter a search term \"([^\"]*)\"$")
     public void i_enter_a_search_term(String arg1) throws Throwable {
-        //System.out.println(arg1);
-
+        //WebElement element = driver.findElement(By.name("q"));
+       // element.sendKeys("apple");
     }
 
     @When("^I click on the search button$")
